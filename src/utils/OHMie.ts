ec017ad4082a9edcf6ea7e6cb985fb481c443dfb
Drondin <drondin596@gmail.com>
2021-05-14 18:36:09 +0200

    Default total to 0

--- src/utils/OHMie.ts
+++ src/utils/OHMie.ts
@@ -1,4 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
 
 import { Ohmie } from '../../generated/schema'
 
@@ -6,6 +7,7 @@ export function loadOrCreateOHMie(addres: Address ): Ohmie{
     let ohmie = Ohmie.load(addres.toHex())
     if (ohmie == null) {
         ohmie = new Ohmie(addres.toHex())
+        ohmie.daiBondTotalDeposit = new BigDecimal(new BigInt(0))
         ohmie.save()
     }
     return ohmie as Ohmie
