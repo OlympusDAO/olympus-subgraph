cc2def3e81aab5a9d43deecab0f6d6806982e75c
Drondin <drondin596@gmail.com>
2021-05-14 15:28:49 +0200

    Add return Type for ohmie helper

--- src/utils/OHMie.ts
+++ src/utils/OHMie.ts
@@ -2,7 +2,7 @@ import { Address } from '@graphprotocol/graph-ts'
 
 import { OHMie } from '../../generated/schema'
 
-export function loadOrCreateOHMie(addres: Address ){
+export function loadOrCreateOHMie(addres: Address ): OHMie{
     let user = OHMie.load(addres.toHex())
     if (user == null) {
       user = new OHMie(addres.toHex())
