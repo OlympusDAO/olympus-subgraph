d19be0adade2e461cc070bd362db43ee59336365
Drondin <drondin596@gmail.com>
2021-05-21 11:11:47 +0200

    Replace adresses with constants

--- src/OlympusERC20.ts
+++ src/OlympusERC20.ts
@@ -6,7 +6,7 @@ import { toDecimal } from "./utils/Decimals"
 import { loadOrCreateOHMie } from "./utils/OHMie"
 import { loadOrCreateTransaction } from "./utils/Transactions"
 import { loadOrCreateRedemption } from './utils/Redemption'
-import { DAIBOND_CONTRACTS, OHMDAISLPBOND_CONTRACT1, OHMDAISLPBOND_CONTRACT2} from './utils/Constants'
+import { DAO_ADDRESS, DISTRIBUTOR_CONTRACT, getBondContracts } from './utils/Constants'
 
 export function handleTransfer(event: Transfer): void {
     let ohmieFrom = loadOrCreateOHMie(event.params.from as Address)
@@ -28,11 +28,8 @@ export function handleTransfer(event: Transfer): void {
     ohmieFrom.ohmBalance = ohmieFrom.ohmBalance.minus(value)
     ohmieFrom.save()
 
-    log.debug("1234567890 TX From {}  TX To {} TX {}   DAI contract {} SLP1 {} SLP2 {}", [ohmieFrom.id, ohmieTo.id, event.transaction.hash.toHex(), DAIBOND_CONTRACTS, OHMDAISLPBOND_CONTRACT1, OHMDAISLPBOND_CONTRACT2])
-    //When the TX comes from the BondContract, update the redeem amount as its not present on the redeem call
-    if ((ohmieFrom.id.includes(DAIBOND_CONTRACTS) || ohmieFrom.id.includes(OHMDAISLPBOND_CONTRACT1) || ohmieFrom.id.includes(OHMDAISLPBOND_CONTRACT2)) && !ohmieTo.id.includes("0xbe731507810c8747c3e01e62c676b1ca6f93242f") && !ohmieTo.id.includes("0x245cc372c84b3645bf0ffe6538620b04a217988b")){
-        log.debug("1234567890 Created redemption {} ", [event.transaction.hash.toHex()])
-
+    //When the TX comes from any BondContract and is not directed to distributor ot DAO, update the redeem amount as its not present on the redeem call. This could be solved with an event.
+    if (getBondContracts().includes(ohmieFrom.id) && !ohmieTo.id.includes(DISTRIBUTOR_CONTRACT) && !ohmieTo.id.includes(DAO_ADDRESS)){
         let redemption = loadOrCreateRedemption(event.transaction.hash as Address)
         redemption.amount = value
         redemption.save()
