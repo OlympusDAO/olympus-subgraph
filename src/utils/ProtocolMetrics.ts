d217b4e9c8b23e68b460ea18ca239ac211ebe61a
Drondin <drondin596@gmail.com>
2021-07-03 15:59:10 +0000

    Fix pair pricing

--- src/utils/ProtocolMetrics.ts
+++ src/utils/ProtocolMetrics.ts
@@ -1,14 +1,14 @@
-import { Address, BigDecimal, BigInt} from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt, log} from '@graphprotocol/graph-ts'
 import { OlympusERC20 } from '../../generated/OlympusStakingV1/OlympusERC20';
 import { CirculatingSupply } from '../../generated/OlympusStakingV1/CirculatingSupply';
-import { BondingCalculator } from '../../generated/OlympusStakingV1/BondingCalculator';
 import { ERC20 } from '../../generated/OlympusStakingV1/ERC20';
+import { UniswapV2Pair } from '../../generated/OlympusStakingV1/UniswapV2Pair';
 
 import { ProtocolMetric, Transaction } from '../../generated/schema'
-import { BONDING_CALCULATOR, BONDING_CALCULATOR_BLOCK, CIRCULATING_SUPPLY_CONTRACT, CIRCULATING_SUPPLY_CONTRACT_BLOCK, DAIBOND_CONTRACTS1, ERC20DAI_CONTRACT, ERC20FRAX_CONTRACT, ERC20OHM_DAI_CONTRACT, ERC20OHM_FRAX_CONTRACT, OHM_ERC20_CONTRACT, SOHM_ERC20_CONTRACT, STAKING_CONTRACT_V1, STAKING_CONTRACT_V2, TREASURY_ADDRESS, TREASURY_ADDRESS_V2, TREASURY_ADDRESS_V2_BLOCK } from './Constants';
+import { CIRCULATING_SUPPLY_CONTRACT, CIRCULATING_SUPPLY_CONTRACT_BLOCK, ERC20DAI_CONTRACT, ERC20FRAX_CONTRACT, OHM_ERC20_CONTRACT, STAKING_CONTRACT_V1, STAKING_CONTRACT_V2, SUSHI_OHMDAI_PAIR, TREASURY_ADDRESS, TREASURY_ADDRESS_V2, TREASURY_ADDRESS_V2_BLOCK, UNI_OHMFRAX_PAIR, UNI_OHMFRAX_PAIR_BLOCK } from './Constants';
 import { dayFromTimestamp } from './Dates';
 import { toDecimal } from './Decimals';
-import { getOHMUSDRate } from './Price';
+import { getOHMUSDRate, getPairUSD } from './Price';
 
 export function loadOrCreateProtocolMetric(timestamp: BigInt): ProtocolMetric{
     let dayTimestamp = dayFromTimestamp(timestamp);
@@ -61,32 +61,48 @@ export function updateProtocolMetrics(transaction: Transaction): void{
     pm.totalValueLocked = pm.totalOHMstaked.times(pm.ohmPrice)
 
     //Treasury RFV and MV
-    if(transaction.blockNumber.gt(BigInt.fromString(BONDING_CALCULATOR_BLOCK))){
-        let bondingCalculator_contract = BondingCalculator.bind(Address.fromString(BONDING_CALCULATOR))
-        let daiERC20 = ERC20.bind(Address.fromString(ERC20DAI_CONTRACT))
-        let fraxERC20 = ERC20.bind(Address.fromString(ERC20FRAX_CONTRACT))
-        let ohmdaiERC20 = ERC20.bind(Address.fromString(ERC20OHM_DAI_CONTRACT))
-        let ohmfraxERC20 = ERC20.bind(Address.fromString(ERC20OHM_FRAX_CONTRACT))
+    let daiERC20 = ERC20.bind(Address.fromString(ERC20DAI_CONTRACT))
+    let fraxERC20 = ERC20.bind(Address.fromString(ERC20FRAX_CONTRACT))
+    let ohmdaiPair = UniswapV2Pair.bind(Address.fromString(SUSHI_OHMDAI_PAIR))
+    let ohmfraxPair = UniswapV2Pair.bind(Address.fromString(UNI_OHMFRAX_PAIR))
 
-        let treasury_address = TREASURY_ADDRESS;
-        if(transaction.blockNumber.gt(BigInt.fromString(TREASURY_ADDRESS_V2_BLOCK))){
-            treasury_address = TREASURY_ADDRESS_V2;
-        }
-
-        let daiBalance = daiERC20.balanceOf(Address.fromString(treasury_address))
-        let fraxBalance = fraxERC20.balanceOf(Address.fromString(treasury_address))
-        let ohmdaiBalance = ohmdaiERC20.balanceOf(Address.fromString(treasury_address))
-        let ohmfraxBalance = ohmfraxERC20.balanceOf(Address.fromString(treasury_address))
-
-        let ohmdai_value = bondingCalculator_contract.valuation(Address.fromString(ERC20OHM_DAI_CONTRACT), ohmdaiBalance)
-        let ohmfrax_value = bondingCalculator_contract.valuation(Address.fromString(ERC20OHM_FRAX_CONTRACT), ohmfraxBalance)
-
-        pm.treasuryMarketValue = toDecimal(daiBalance.plus(fraxBalance).plus(ohmdai_value).plus(ohmfrax_value),18)
-
-        let ohmdai_rfv = ohmdai_value.times(BigInt.fromI32(3)).div(BigInt.fromI32(100))
-        let ohmfrax_rfv = ohmfrax_value.times(BigInt.fromI32(3)).div(BigInt.fromI32(100))
-        pm.treasuryRiskFreeValue = toDecimal(daiBalance.plus(fraxBalance).plus(ohmdai_rfv).plus(ohmfrax_rfv),18)
+    let treasury_address = TREASURY_ADDRESS;
+    if(transaction.blockNumber.gt(BigInt.fromString(TREASURY_ADDRESS_V2_BLOCK))){
+        treasury_address = TREASURY_ADDRESS_V2;
     }
+
+    let daiBalance = daiERC20.balanceOf(Address.fromString(treasury_address))
+    let fraxBalance = fraxERC20.balanceOf(Address.fromString(treasury_address))
+    let ohmdaiBalance = ohmdaiPair.balanceOf(Address.fromString(treasury_address))
+    let ohmdai_value = getPairUSD(ohmdaiBalance, SUSHI_OHMDAI_PAIR)
+
+    let ohmfraxBalance = BigInt.fromI32(0)
+    let ohmfrax_value = BigDecimal.fromString("0")
+    if(transaction.blockNumber.gt(BigInt.fromString(UNI_OHMFRAX_PAIR_BLOCK))){
+        ohmfraxBalance = ohmfraxPair.balanceOf(Address.fromString(treasury_address))
+        ohmfrax_value = getPairUSD(ohmfraxBalance, UNI_OHMFRAX_PAIR)
+    }
+
+    log.info("Treasury ohmdai_value {} ohmfrax_value {}", [ohmdai_value.toString(), ohmfrax_value.toString()])
+
+    let ohmdai_rfv = ohmdai_value.times(BigDecimal.fromString("0.3"))
+    let ohmfrax_rfv = ohmfrax_value.times(BigDecimal.fromString("0.3"))
+    log.info("Treasury ohmdai_rfv {} ohmfrax_rfv {}", [ohmdai_rfv.toString(), ohmfrax_rfv.toString()])
+
+
+    let stableValue = daiBalance.plus(fraxBalance)
+    let stableValueDecimal = toDecimal(stableValue, 18)
+    log.info("Treasury Stable Balance {}", [stableValue.toString()])
+    log.info("Treasury Stable Double Balance {}", [stableValueDecimal.toString()])
+
+    let lpValue = ohmdai_value.plus(ohmfrax_value)
+    log.info("Treasury LPV Balance {}", [lpValue.toString()])
+
+    let rfvLpValue = ohmdai_rfv.plus(ohmfrax_rfv)
+    log.info("Treasury RFV Balance {}", [rfvLpValue.toString()])
+
+    pm.treasuryMarketValue = stableValueDecimal.plus(lpValue)
+    pm.treasuryRiskFreeValue = stableValueDecimal.plus(rfvLpValue)
 
     pm.save()
     
